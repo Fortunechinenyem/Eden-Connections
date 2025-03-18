4b9541b58ccd0d68038f0f1779e7ee276350bd9e
Fortunechinenyem <fortunechinenyem@gmail.com>
2025-03-18 18:08:41 +0100

    setup

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "../styles/globasl.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Eden-Connections",
+  description:
+    "A christian Dating App for matured single and those who want to give love another chance.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
